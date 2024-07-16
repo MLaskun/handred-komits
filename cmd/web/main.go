3721cb820d03628b2e51e358e43fcf197781794c
Mateusz Laska <mateo13311@gmail.com>
2024-07-16 23:36:59 +0200

    Add docker compose and change folders structure

---
+++ cmd/web/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type application struct {
+	logger *slog.Logger
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "root:handred@tcp(mysql:3306)/handred", "MySQL data source")
+	flag.Parse()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	app := &application{
+		logger: logger,
+	}
+
+	err = http.ListenAndServe(*addr, app.routes())
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
+}
---
+++ cmd/web/routes.go
@@ -0,0 +1,9 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	return mux
+}
