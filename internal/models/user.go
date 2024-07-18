189ae913ab2888b4d37e74a91bc876ca9dedd5b3
Mateusz Laska <mateo13311@gmail.com>
2024-07-18 22:49:57 +0200

    Inserting new user

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,4 +1,30 @@
 package models
 
+import "database/sql"
+
 type User struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
+
+type UserModel struct {
+	DB *sql.DB
+}
+
+func (m *UserModel) Insert(username string, password string, email string) (int, error) {
+	stmt := `INSERT INTO users (username, password, email)`
+
+	result, err := m.DB.Exec(stmt, username, password, email)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
